221802dbe959f1c415529e48456a93e4e63f4b6c
alfcastillo90 <cacr1990@gmail.com>
2020-05-23 00:02:37 -0400

    [ADD]: first commit and first steps with js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HolaMundo(age, nombre) {
+  const presentacion = (
+  <div>
+    <h2>Hi, I'm {nombre} </h2>
+    <h3>I'm {age} years old</h3>
+  </div>);
+  
+  return presentacion;
+}
+
 function App() {
+  const nombre = "Carlos Alfredo Castillo Rodríguez";
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello!. This is my first react web application</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        { HolaMundo(29, nombre) }
         <a
           className="App-link"
           href="https://reactjs.org"
